6add3d60f29052e712497ef1b229177fce71c81e
Amal <amalsaga@gmail.com>
2021-06-26 16:09:14 +0530

    modifications

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import numpy
 from keras.models import load_model
+import sys
 
 model = load_model('textmodel.h5')
 print(model.summary())
 
+filename = "wonderland.txt"
+raw_text = open(filename, 'r', encoding='utf-8').read()
+raw_text = raw_text.lower()
+
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+
+n_chars = len(raw_text)
+n_vocab = len(chars)
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print ("Seed:")
+print( "\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print( "\nDone.")
