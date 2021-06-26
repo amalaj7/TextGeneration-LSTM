b57b21afa32934ec7a444743c64c5904ed475d8d
Amal <amalsaga@gmail.com>
2021-06-26 16:16:25 +0530

    text gen

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+# Doing necessary imports
 import numpy
 from keras.models import load_model
 import sys
 
+# to load the pretrained model
 model = load_model('textmodel.h5')
 #print(model.summary())
 
+# Dataset - Alice in WonderLand Story
 filename = "wonderland.txt"
 raw_text = open(filename, 'r', encoding='utf-8').read()
 raw_text = raw_text.lower()
 
+# Characters to Integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+
 n_chars = len(raw_text)
 n_vocab = len(chars)
 
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
+
+# prepare the dataset of input to output pairs encoded as integers
 seq_length = 100
 dataX = []
 dataY = []
@@ -28,11 +35,9 @@ n_vocab = len(chars)
 int_to_char = dict((i, c) for i, c in enumerate(chars))
 start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
-# print ("Seed:")
-# print( "\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 
 
-# generate characters
+# generate new characters
 for i in range(1000):
 	x = numpy.reshape(pattern, (1, len(pattern), 1))
 	x = x / float(n_vocab)
